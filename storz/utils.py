b01d5e25d726430867f3ef5da2f15b926c477c6a
Paul Tagliamonte <paultag@debian.org>
2013-02-09 18:13:39 -0500

    Adding in utils; starting to build some stuff out 4real

---
+++ storz/utils.py
@@ -0,0 +1,29 @@
+# Copyright (c) Paul R. Tagliamonte <paultag@debian.org>, 2013 under the terms
+# and conditions of the GPL-2+.
+# -*- coding: utf-8 -*-
+
+import shlex
+import subprocess
+
+
+def run_command(command):
+    """
+    Run a synchronized command. The argument must be a list of arguments.
+    Returns a triple (stdout, stderr, exit_status)
+
+    If there was a problem to start the supplied command, (None, None, -1) is
+    returned
+    """
+
+    if not isinstance(command, list):
+        command = shlex.split(command)
+    try:
+        pipe = subprocess.Popen(command,
+                                shell=False,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+    except OSError as e:
+        return (None, None, -1)
+
+    (output, stderr) = pipe.communicate()
+    return (output, stderr, pipe.returncode)
--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from storz.wrapper import generate_analysis
-import sys
-
-
-obj = generate_analysis('lintian', '1.0', sys.argv[1])
-print obj
