aae63564dc08c99c105707f548c0af88ea7c24f4
Paul Tagliamonte <paultag@debian.org>
2013-02-09 19:35:08 -0500

    Spacing sutff out; headers.

--- storz/__init__.py
+++ storz/__init__.py
@@ -1,2 +1,6 @@
+# Copyright (c) Paul R. Tagliamonte <paultag@debian.org>, 2013 under the terms
+# and conditions of the GPL-2+.
+# -*- coding: utf-8 -*-
+
 __appname__ = "storz"
 __version__ = "0.1"
--- storz/errors.py
+++ storz/errors.py
@@ -1,4 +1,6 @@
-
+# Copyright (c) Paul R. Tagliamonte <paultag@debian.org>, 2013 under the terms
+# and conditions of the GPL-2+.
+# -*- coding: utf-8 -*-
 
 class StorzError(Exception):
     pass
