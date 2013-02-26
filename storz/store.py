d55810b6810f17bdb6a154e1ef46e61f80c8c5ec
Paul R. Tagliamonte <paultag@debian.org>
2013-02-25 20:34:02 -0500

    things.

--- storz/store.py
+++ storz/store.py
@@ -2,6 +2,9 @@ from storz.decompress import digest_firehose_tree
 from storz.core import db
 
 
-def store(r):
+def store(r, when):
     obj = digest_firehose_tree(r)
-    db.results.insert(obj, safe=True)
+    db.results.insert({
+        "result": obj,
+        "when": when
+    }, safe=True)
